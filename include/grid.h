459f7bcf95b86a7b47bcb1bc7c2920447fea6e5e
Marius Melzer <marius.melzer@rasumi.net>
2013-01-17 12:03:49 +0100

    add grid.h and grid.cu

---
+++ include/grid.h
@@ -0,0 +1,52 @@
+#ifndef GRID_H
+#define GRID_H 
+
+#include "datatypes.h"
+
+//***************
+// DATASTRUCTURES 
+//***************
+
+//Axis Aligned Bounding Box
+struct Aabb {
+	PointCu min;
+	PointCu max;
+};
+
+struct GridCell {
+	int* prismIdxList;
+	int length;
+};
+
+struct Grid {
+	int dimX;
+	int dimY;
+	int dimZ;
+	Aabb aabb;
+	GridCell* cellList;
+};
+
+//***********
+// BUILD GRID
+//***********
+
+/**
+ * @brief calculate bounding boxes for numPrims prisms
+ **/
+void calcAabbs(const PrismCu *prisms, Aabb *aabbs, int numPrisms);
+
+/**
+ * @brief build grid based on the geometry-independent bounding boxes
+ **/
+void buildGrid(Grid *grid, const Aabb *aabbs, int numAabbs);
+
+//************
+// SEARCH GRID
+//************
+
+/**
+ * @brief filter all geometric objects by whether they "might" be hit by the ray
+ **/
+int filter(const Grid *grid, const RayCu *ray, int* results);
+
+#endif /* GRID_H */
