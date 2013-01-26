05c6d5856e100fadd8e10ce410f99d41cce67b71
Carlchristian Eckert <s0500037@joker.cm.cluster>
2013-01-26 16:21:00 +0100

    * importance-sampling function included * testdata extended (for importance sampling) * importance sampling doesn't run properly (causes infinite run)

--- include/naive_ray_propagation.h
+++ include/naive_ray_propagation.h
@@ -16,6 +16,7 @@
 #include "testdata_transposed.h"
 
 #define TEST_VALUES true
+#define USE_IMPORTANCE false
 #define SMALL 1E-06
 
 #define CUDA_CALL(x) do { if((x) != cudaSuccess) { \
@@ -254,6 +255,93 @@ __device__ float naivePropagation(double x_pos, double y_pos, double z_pos, doub
 	return gain/(distance_total*distance_total);
 }
 
+#if USE_IMPORTANCE==true
+__device__ void importf(curandState localstate, int point, int mesh_start, double *importance, int *N_rays, double *p_in, double *n_x, double *n_y, int *n_p, int *neighbors, int *forbidden, int *cell_type, double *beta_v, double *center_x, double *center_y, int *surface, int NumRays)
+{
+    int i_t, i_z, Rays_dump=0, rays_left, i_r, rand_t, rand_z;
+    double sum_phi=0.0, surf_tot=0.0;
+    double distance, x_pos, y_pos, z_pos;
+    double prop;
+
+//    calculate the gain from the centers of each of the boxes to the observed point
+//    calculate the gain and make a "mapping"
+//    receipt: pick the point in the center of one cell, 
+//    calculate the gain from this point to the observed point,
+//    estimate the inner part of the Phi_ASE - Integral,
+//    scale the amount of rays proportionally with it
+//    sum the amount of rays and scale it to Int=1, which gives the inverse weights
+//    the number of rays is determined via floor(), with ceil(), zero-redions could be added
+
+//    use the routine "propagation"!, test: no reflections, just exponential
+    x_pos = p_in[point];
+    y_pos = p_in[point+size_p];
+    z_pos = mesh_start * z_mesh;
+
+    for (i_t=0;i_t<N_cells;i_t++)
+    {
+
+        for (i_z=0;i_z<(mesh_z-1);i_z++) //remember the definition differences MatLab/C for indices
+        {
+//            at this point replace the following routine with propagation(...)
+//            later expand this with the beta/tau values...
+			prop = naivePropagation(center_x[i_t], center_y[i_t], z_mesh*(i_z+0.5),  x_pos, y_pos, z_pos, i_t, i_z, p_in, n_x, n_y, n_p, neighbors, forbidden , cell_type, beta_v);
+			// Propagation vom Zentrum jedes Prismas zu jedem Samplepunkt 
+			//
+//			prop = propagation(center_x[i_t], center_y[i_t], z_mesh*(i_z+0.5), x_pos, y_pos, z_pos, i_t, i_z);
+            importance[i_t + i_z*N_cells] = beta_v[i_t+i_z*N_cells]*(prop);
+            sum_phi += importance[i_t + i_z*N_cells];
+
+        }
+        surf_tot += surface[i_t];
+    }
+
+//    now calculate the number of rays
+    for (i_t=0;i_t<N_cells;i_t++)
+    {
+        for (i_z=0;i_z<(mesh_z-1);i_z++) //remember the definition differences MatLab/C for indices
+        {
+//            this is the amount of the sampled rays out of the cells
+            N_rays[i_t + i_z*N_cells] = (int)(floor(importance[i_t + i_z*N_cells]/sum_phi*NumRays));
+
+            Rays_dump +=  N_rays[i_t + i_z*N_cells];
+        }
+    }
+
+    rays_left = NumRays-Rays_dump;
+//    distribute the remaining not distributed rays randomly
+    if ((rays_left)>0)
+    {
+        for (i_r=0;i_r<rays_left;i_r++)
+        {
+            rand_t = (int )(curand_uniform(&localstate)*N_cells);
+            rand_z = (int )(curand_uniform(&localstate)*(mesh_z-1));
+            N_rays[rand_t + rand_z*N_cells]++;
+        }
+    }
+
+//    now think about the mount of rays which would come out of this volume(surface)
+//    dividing this number with the new amount of rays gives the final importance weight for this area!
+    for (i_t=0;i_t<N_cells;i_t++)
+    {
+        for (i_z=0;i_z<(mesh_z-1);i_z++) //remember the definition differences MatLab/C for indices
+        {
+//            this is the amount of the sampled rays out of the cells
+            if (N_rays[i_t + i_z*N_cells]>0)
+            {
+                importance[i_t + i_z*N_cells] = NumRays*surface[i_t]/surf_tot/N_rays[i_t + i_z*N_cells];
+//                importance[i_t + i_z*N_cells] = NumRays*surface[i_t]/surf_tot;
+            }
+            else
+            {
+                importance[i_t + i_z*N_cells] = 0; // case of beta of this point == 0 e.g.
+            }
+        }
+    }
+}
+#endif
+
+
+
 __global__ void setupKernel ( double host_sigma_e, double host_sigma_a, int host_clad_num, int host_clad_abs, double host_N_tot, int host_N_cells, double host_z_mesh, int host_mesh_z, int host_size_p ){
 	sigma_e = host_sigma_e;	
 	sigma_a = host_sigma_a;
@@ -267,6 +355,19 @@ __global__ void setupKernel ( double host_sigma_e, double host_sigma_a, int host
 	//printf("Sigma_e in setup=%f\tSigma_eHost=%f\n",sigma_e,host_sigma_e);
 } 
 
+#if USE_IMPORTANCE==true
+__global__ void importanceKernel( curandState *globalState, double *p_in, double *n_x, double *n_y, int *n_p, int *neighbors, int *forbidden, int* cell_type, int host_size_t, double* beta_v, double *importance, int *N_rays, double *center_x, double *center_y, int *surface, int NumRays) {
+	int id = threadIdx.x + blockIdx.x * blockDim.x;
+	for(int i=0; i< host_size_t; ++i){
+		for(int j=0; j< mesh_z; ++j){
+			importf(globalState[id], i,j, importance, N_rays, p_in, n_x, n_y, n_p, neighbors, forbidden, cell_type, beta_v, center_x, center_y,surface, NumRays);
+
+		}
+	}
+	
+}
+#endif
+
 __global__ void testKernel (  ){
 	printf("\nSigma_e=%.30f",sigma_e);
 	printf("\nSigma_a=%.30f",sigma_a);
@@ -276,7 +377,11 @@ __global__ void testKernel (  ){
 
 
 // does the raytracing for a single ray (randomly generated) and a single (given) Vertex
+#if USE_IMPORTANCE==true
+__global__ void raytraceStep( curandStateMtgp32* globalState, float* phi, int point2D, int level, int iterations, double *p_in, double *n_x, double *n_y, int *n_p, int *neighbors, int *forbidden, int* t_in, int* cell_type, int host_size_t, double* beta_v, double *importance) {
+#else
 __global__ void raytraceStep( curandStateMtgp32* globalState, float* phi, int point2D, int level, int iterations, double *p_in, double *n_x, double *n_y, int *n_p, int *neighbors, int *forbidden, int* t_in, int* cell_type, int host_size_t, double* beta_v) {
+#endif
 	int id = threadIdx.x + blockIdx.x * blockDim.x;
 	double gain = 0.;
 	const int endPointX = p_in[point2D];
@@ -319,8 +424,11 @@ __global__ void raytraceStep( curandStateMtgp32* globalState, float* phi, int po
 	// do the multiplication just at the end of all iterations
 	// (gives better numeric behaviour)
 	gain *=  beta_v[startTriangle + N_cells*startLevel]; //@TODO: why is there a beta_v reference in the sequential-code?
-
-	atomicAdd(&(phi[point2D + level*size_p]),float(gain)); //@TODO: importance-value 
+#if USE_IMPORTANCE==true
+	atomicAdd(&(phi[point2D + level*size_p]),float(gain*importance[startTriangle + N_cells*startLevel]));  
+#else
+	atomicAdd(&(phi[point2D + level*size_p]),float(gain)); 
+#endif
 }
 
 //----------------------------------------------------
@@ -332,6 +440,21 @@ float runNaiveRayPropagation(std::vector<double> *ase){
 	double  *p_in, *n_x, *n_y, *beta_v;
 	float *phi;
 	int *forbidden, *n_p, *neighbors, *t_in, *cell_type;
+
+
+#if USE_IMPORTANCE==true
+	curandState *devStates;
+	int *N_rays, *surface;
+	double *center_x, *center_y, *importance;
+	double host_importance[host_size_p * (host_mesh_z+1)];
+	int host_N_rays[host_size_t * host_mesh_z];
+	for(int i=0;i<host_size_p*(host_mesh_z+1);++i){
+		host_importance[i] = 0.;
+	}
+	for(int i=0;i<host_size_t*host_mesh_z;++i){
+		host_N_rays[i] = 0;
+	}
+#endif
 
 	int host_N_cells = host_size_t;
 
@@ -347,7 +470,7 @@ float runNaiveRayPropagation(std::vector<double> *ase){
 	cudaEventCreate(&stop);
 
 	// GPU Raytracing
-	unsigned raysPerSample = 102400000; //
+	unsigned raysPerSample = 102400; //
 	int threads = 256;
 	int blocks = 200;
 	int iterations = float(raysPerSample) / (blocks * threads);
@@ -363,7 +486,13 @@ float runNaiveRayPropagation(std::vector<double> *ase){
 
 		//Create constant values on GPU
 		setupKernel<<<1,1>>>(host_sigma_e, host_sigma_a, host_clad_num, host_clad_abs, host_N_tot, host_N_cells, host_z_mesh, host_mesh_z, host_size_p); //@OPTIMIZE: initialize the constants as constants...
+
+		// Start random setup kernel
+		fprintf(stderr, "C Init GPU Random number\n");
+
+
 		cudaThreadSynchronize();
+
 		//testKernel<<<1,1>>>();
 
 		// Memory allocation on device (mexFunction Variables)
@@ -377,6 +506,15 @@ float runNaiveRayPropagation(std::vector<double> *ase){
 		CUDA_CHECK_RETURN(cudaMalloc(&cell_type,host_size_t * host_mesh_z * sizeof(int)));
 		CUDA_CHECK_RETURN(cudaMalloc(&beta_v,host_size_t * host_mesh_z * sizeof(double)));
 		CUDA_CHECK_RETURN(cudaMalloc(&phi,host_size_p * (host_mesh_z +1) * sizeof(float)));
+#if USE_IMPORTANCE==true
+		CUDA_CHECK_RETURN(cudaMalloc(&importance,host_size_p * (host_mesh_z +1) * sizeof(double)));
+		CUDA_CHECK_RETURN(cudaMalloc(&center_x,host_size_t * (host_mesh_z) * sizeof(double)));
+		CUDA_CHECK_RETURN(cudaMalloc(&center_y,host_size_t * (host_mesh_z) * sizeof(double)));
+		CUDA_CHECK_RETURN(cudaMalloc(&N_rays,host_size_p * (host_mesh_z +1) * sizeof(int)));
+		CUDA_CHECK_RETURN(cudaMalloc(&surface,host_size_p * sizeof(int)));
+		CUDA_CHECK_RETURN(cudaMalloc(&devStates, iterations * threads * blocks * sizeof(curandState)));
+
+#endif
 
 		// Copy data from host to device (mex Function Variables)
 		CUDA_CHECK_RETURN(cudaMemcpy(p_in, host_p_in, 2 * host_size_p * sizeof(double), cudaMemcpyHostToDevice));
@@ -389,6 +527,13 @@ float runNaiveRayPropagation(std::vector<double> *ase){
 		CUDA_CHECK_RETURN(cudaMemcpy(cell_type,host_cell_type, host_size_t *  host_mesh_z * sizeof(int), cudaMemcpyHostToDevice));
 		CUDA_CHECK_RETURN(cudaMemcpy(beta_v, host_beta_v, host_size_t * host_mesh_z * sizeof(double), cudaMemcpyHostToDevice));
 		CUDA_CHECK_RETURN(cudaMemcpy(phi, hostPhi, host_size_p * (host_mesh_z+1) * sizeof(float), cudaMemcpyHostToDevice));
+#if USE_IMPORTANCE==true
+		CUDA_CHECK_RETURN(cudaMemcpy(importance, host_importance, host_size_p * (host_mesh_z+1) * sizeof(double), cudaMemcpyHostToDevice));
+		CUDA_CHECK_RETURN(cudaMemcpy(center_x, host_center_x, host_size_t * host_mesh_z * sizeof(double), cudaMemcpyHostToDevice));
+		CUDA_CHECK_RETURN(cudaMemcpy(center_y, host_center_y, host_size_t * host_mesh_z * sizeof(double), cudaMemcpyHostToDevice));
+		CUDA_CHECK_RETURN(cudaMemcpy(N_rays, host_N_rays, host_size_p * (host_mesh_z+1) * sizeof(int), cudaMemcpyHostToDevice));
+		CUDA_CHECK_RETURN(cudaMemcpy(surface, host_surface, host_size_p * sizeof(int), cudaMemcpyHostToDevice));
+#endif
 	}
 
 	// Generating Random Numbers
@@ -407,6 +552,13 @@ float runNaiveRayPropagation(std::vector<double> *ase){
 		CURAND_CALL(curandMakeMTGP32KernelState(devMTGPStates, mtgp32dc_params_fast_11213, devKernelParams, blocks, 1234)); 
 	}
 
+#if USE_IMPORTANCE==true
+		fprintf(stderr, "\nStarting the Importance Sampling\n");
+		random_setup_kernel <<< blocks, threads >>> ( devStates, time(NULL) );
+		cudaThreadSynchronize();
+
+		importanceKernel<<< blocks*iterations, threads>>>(devStates, p_in, n_x, n_y, n_p, neighbors, forbidden, cell_type, host_size_t, beta_v, importance, N_rays, center_x, center_y, surface, blocks*threads*iterations*host_size_p*(host_mesh_z+1) );
+#endif
 	// start the Kernels
 	{
 		fprintf(stderr, "\nStarting the Naive Propagation\n");
@@ -415,7 +567,12 @@ float runNaiveRayPropagation(std::vector<double> *ase){
 		for(int point2D = 0; point2D < host_size_p ; ++point2D){
 			for(int level = 0; level <= host_mesh_z; ++ level){
 				cudaThreadSynchronize();
+#if USE_IMPORTANCE==true
+				raytraceStep<<< blocks, threads >>> ( devMTGPStates, phi, point2D, level, iterations, p_in, n_x, n_y, n_p, neighbors, forbidden, t_in, cell_type, host_size_t, beta_v, importance);
+#else
 				raytraceStep<<< blocks, threads >>> ( devMTGPStates, phi, point2D, level, iterations, p_in, n_x, n_y, n_p, neighbors, forbidden, t_in, cell_type, host_size_t, beta_v);
+
+#endif
 			}
 		}
 
@@ -429,6 +586,10 @@ float runNaiveRayPropagation(std::vector<double> *ase){
 		cudaEventElapsedTime(&runtimeGpu, start, stop);
 
 		CUDA_CHECK_RETURN(cudaMemcpy(hostPhi, phi, host_size_p * (host_mesh_z+1) * sizeof(int), cudaMemcpyDeviceToHost));
+#if USE_IMPORTANCE==true
+		CUDA_CHECK_RETURN(cudaMemcpy(host_importance, importance, host_size_p * (host_mesh_z+1) * sizeof(double), cudaMemcpyDeviceToHost));
+		CUDA_CHECK_RETURN(cudaMemcpy(host_N_rays, N_rays, host_size_p * (host_mesh_z+1) * sizeof(int), cudaMemcpyDeviceToHost));
+#endif
 		for(int i=0; i< host_size_p*(host_mesh_z+1); ++i){
 			//fprintf(stderr, "\nPhi_ase[%d]= %.10f",i, hostPhi[i] / raysPerSample);
 			ase->at(i) = (double(double(hostPhi[i]) / raysPerSample));
@@ -444,6 +605,12 @@ float runNaiveRayPropagation(std::vector<double> *ase){
 		cudaFree(forbidden);
 		cudaFree(n_p);
 		cudaFree(beta_v);
+#if USE_IMPORTANCE==true
+		cudaFree(importance);
+		cudaFree(N_rays);
+		cudaFree(center_x);
+		cudaFree(center_y);
+#endif
 	}
 
 	// print statistics
