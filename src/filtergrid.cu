eddc40407ab661973adf8ad88379fc0541d9a237
Marius Melzer <marius.melzer@rasumi.net>
2013-05-14 16:44:03 +0200

    use DynIntSet in filtergrid for the results tidy up a little

--- src/filtergrid.cu
+++ src/filtergrid.cu
@@ -4,35 +4,11 @@
 
 #include "filtergrid.h"
 #include "geometry_gpu.h"
+#include "dynintset.h"
 
 __device__ int cellIdx(const Grid *grid, int i, int j, int k){
 	return k*grid->dim.x*grid->dim.y+j*grid->dim.x+i;
 }
-
-//__global__ void filter(const Grid *grid, const RayCu *ray, char *results) {
-//  int gx = threadIdx.x;
-//  int gy = threadIdx.y;
-//  int gz = threadIdx.z;
-//
-//  PointCu a, b, c, d;
-//  PrismCu p1, p2;
-//
-//  VectorCu gridAabbDim = createVector(grid->aabb.min, grid->aabb.max);
-//  VectorCu cellDim;
-//  cellDim.x = gridAabbDim.x / grid->dim.x;
-//  cellDim.y = gridAabbDim.y / grid->dim.y;
-//  cellDim.z = gridAabbDim.z / grid->dim.z;
-//
-//  a = createPoint(gx*cellDim.x, gy*cellDim.y, gz*cellDim.z, cellDim.z);
-//  b = createPoint(gx*cellDim.x, gy*cellDim.y, gz*cellDim.z, cellDim.z);
-//  c = createPoint(gx*cellDim.x, gy*cellDim.y, gz*cellDim.z, cellDim.z);
-//  d = createPoint(gx*cellDim.x, gy*cellDim.y, gz*cellDim.z, cellDim.z);
-//
-//  p1.t1 = createTriangle(a, b, c);
-//  p2.t1 = createTriangle(b, c, d);
-//
-//  results[cellIdx(grid, gx, gy, gz)] = (collide_prism_gpu(p1, *ray) != 0) || (collide_prism_gpu(p2, *ray) != 0);
-//}
 
 __device__ int iIdx(const Grid *grid, float x){
 	//assert(x >= grid->aabb.min.x && x <= grid->aabb.max.x);
@@ -52,21 +28,6 @@ __device__ int kIdx(const Grid *grid, float z){
 	return (z - grid->aabb.min.z)/(grid->aabb.max.z - grid->aabb.min.z) * grid->dim.z;
 }
 
-//void cellIdx2ijk(const Grid *grid, int cellIdx, int *i, int *j, int *k){
-//	
-//	int dimXdimY=grid->dim.x*grid->dim.y;
-//	int tmp;
-//	
-//	*k = cellIdx / dimXdimY;
-//	
-//	tmp=cellIdx - *k * dimXdimY;
-//	//*j = (cellIdx - *k * dimXdimY) / grid->dim.x;
-//	*j = tmp / grid->dim.x;
-//	
-//	//*i = cellIdx - *k * dimXdimY - *j* grid->dim.x;
-//	*i = tmp - *j * grid->dim.x;
-//}
-
 __device__ PointCu calcPointOnRay(const RayCu *ray, float t) {
   PointCu p;
 
@@ -75,6 +36,16 @@ __device__ PointCu calcPointOnRay(const RayCu *ray, float t) {
   p.z = ray->P.z + t*ray->direction.z;
   
   return p;
+}
+
+__device__ int4 calcCellpos(const Grid *grid, PointCu p) {
+  int4 result;
+
+  result.x = ((int) p.x) % grid->dim.x;
+  result.y = ((int) p.y) % grid->dim.y;
+  result.z = ((int) p.z) % grid->dim.z;
+
+  return result;
 }
 
 __device__ bool nextIntersection(const Grid *grid, const RayCu *ray, float *t, PointCu p) {
@@ -117,58 +88,8 @@ __device__ bool nextIntersection(const Grid *grid, const RayCu *ray, float *t, P
   return true;
 }
 
-__device__ int4 calcCellpos(const Grid *grid, PointCu p) {
-  int4 result;
-
-  result.x = ((int) p.x) % grid->dim.x;
-  result.y = ((int) p.y) % grid->dim.y;
-  result.z = ((int) p.z) % grid->dim.z;
-
-  return result;
-}
-
-__device__ int* cuRealloc(int *old, int *size) {
-  int oldSize = *size;
-  int* res = (int*) malloc(oldSize * 2);
-  *size *= 2;
-
-  for(int i=0; i<oldSize; ++i)
-    res[i] = old[i];
-
-  free(old);
-  return res;
-}
-
-__device__ bool isDuplicate(int *a, int length, int o) {
-  for(int i=0; i<length; ++i) {
-    if(a[i] == o) return true;
-  }
-  return false;
-}
-
-__device__ int* addPrismsToResult(const GridCell *cell, int* results, int *results_count, int *results_size) {
-  int limit = *results_count + cell->length;
-  if(limit >= *results_size) {
-    while(limit >= *results_size) *results_size += 256;
-    results = cuRealloc(results, results_size);
-  }
-
-  int added = 0;
-  for(int i=0; i<cell->length; ++i) {
-    int current = cell->prismIdxList[i];
-    if(!isDuplicate(results, *results_count, current)) {
-      results[*results_count + added] = current;
-      added++;
-    }
-  }
-  *results_count += added;
-  return results;
-}
-
-__device__ int* filter(const Grid *grid, const RayCu *ray, int* result_size) {
-  int *results = (int*) malloc(256 * sizeof(int));
-  int results_count = 0;
-  int results_size = 256;
+__device__ DynIntSet filter(const Grid *grid, const RayCu *ray, size_t* results_count) {
+  DynIntSet results(256); // dynamically growing array with unique entries
 
   float t = 0;
   PointCu p;
@@ -176,9 +97,8 @@ __device__ int* filter(const Grid *grid, const RayCu *ray, int* result_size) {
     p = calcPointOnRay(ray, t);
     int4 cellpos = calcCellpos(grid, p);
     GridCell cell = grid->cellList[cellIdx(grid, cellpos.x, cellpos.y, cellpos.z)];
-    results = addPrismsToResult(&cell, results, &results_count, &results_size); // uses sometimes realloc and changes results, results_count and results_size
+    results.push(cell.prismIdxList, cell.length);
   } while(nextIntersection(grid, ray, &t, p)); // changes t, but not p (is being done in next loop)
 
-  *result_size = results_count;
   return results;
 }
