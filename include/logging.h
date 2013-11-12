6468139629f835bc5307e26793e07ed30247f44f
Carlchristian Eckert <Carli-Eckert@gmx.de>
2013-11-12 14:16:39 +0100

    * logging function dout is now available

---
+++ include/logging.h
@@ -0,0 +1,27 @@
+#ifndef OCTRACE_LOGGING_H
+#define OCTRACE_LOGGING_H
+
+#include <iostream>
+
+#define V_QUIET 0
+#define V_ERROR 1
+#define V_WARNING 2
+#define V_INFO 4
+#define V_STAT 8
+#define V_DEBUG 16
+
+#define COLOR_ERROR 31
+#define COLOR_WARN 31
+#define COLOR_INFO 30
+#define COLOR_STATISTIC 30
+#define COLOR_DEBUG 36
+
+extern unsigned verbosity;
+
+struct nullstream : std::ostream {
+  nullstream() : std::ostream(0) { }
+};
+
+std::ostream& dout(unsigned activation_level);
+
+#endif
