c09dad092f5d38e965a9a84b049dc6af1ed0b200
Carlchristian Eckert <Carli-Eckert@gmx.de>
2013-11-12 22:30:55 +0100

    * file progressbar now available

--- include/progressbar.h
+++ include/progressbar.h
@@ -2,8 +2,10 @@
 #define PROGRESS_BAR_H
 
 #include <ctime>
+#include <string>
 void simpleProgressBar(unsigned part, unsigned full);
 void fancyProgressBar(unsigned part, unsigned full, unsigned length, time_t starttime);
+void fileProgressBar(const unsigned nTotal, const std::string path);
 
 
 #endif /* PROGRESS_BAR_H */
--- src/progressbar.cu
+++ src/progressbar.cu
@@ -1,8 +1,10 @@
 #include "progressbar.h"
-#include <stdio.h>
 #include <unistd.h>
 #include <ctime>
 #include <logging.h>
+#include <cmath>
+#include <iomaip>
+#include <ofstream>
 
 void simpleProgressBar(unsigned part, unsigned full){
 	unsigned length = 80;
@@ -38,3 +40,63 @@ void fancyProgressBar(unsigned part, unsigned full, unsigned length, time_t star
 	int timeRemaining = timeTotal-timeSpent;
 	dout(V_INFO | V_NOLABEL) << "] " << int(percentage*100) << "% (" << part+1 << "/" << full << ") after " << int(timeSpent) << "s (" << timeTotal << "s total, " << timeRemaining << "remaining)" << std::flush; 
 }
+
+
+void fileProgressBar(unsigned nTotal, std::string path){
+	unsigned length = 50;
+	static unsigned part = 0;
+	static unsigned progress = 0;
+	static const time_t starttime = time(0);
+	static const unsigned fillwidthPart = unsigned(1+log10(nTotal));
+	static std::ofstream filestream;
+	static std::streampos currentPositionOfProgress,endOfProgressBar,maximalEndOfFile = 0;
+
+	++part;
+	const float percentage = float(part) / float(nTotal);
+
+	if(!filestream.is_open()){
+		filestream.open(path.c_str());
+	}
+
+	// set up the empty progress bar
+	if(part == 1){
+		filestream << "Progress: [";
+		currentPositionOfProgress = filestream.tellp();
+		for(unsigned i=0;i<length;++i){
+			filestream << " ";
+		}
+		filestream << "] ";
+		endOfProgressBar = filestream.tellp();
+		maximalEndOfFile = endOfProgressBar;
+	}
+
+	// write progressbar
+	while(unsigned(percentage*length) > progress){
+		++progress;
+		filestream.seekp(currentPositionOfProgress);
+		filestream << "#";
+		currentPositionOfProgress = filestream.tellp();
+	}
+
+	// write progress in percent
+	filestream.seekp(endOfProgressBar);
+	filestream << std::setfill(' ') << std::setw(3) << unsigned(percentage*100) << "% (" << std::setfill(' ') << std::setw(fillwidthPart) << part << "/" << nTotal << ")" << std::endl;
+
+	// go to next line and write time
+	const double timeSpent = difftime(time(0),starttime);
+	const int timeTotal = timeSpent/percentage;
+	const int timeRemaining = timeTotal-timeSpent;
+	filestream << "Runtime " << int(timeSpent) << "s (" << timeTotal << "s total, " << timeRemaining << "s remaining)" << std::flush;
+
+	// append empty characters to overwrite possible dangling characters
+	while(filestream.tellp() < maximalEndOfFile){
+		filestream << " ";
+	}
+	maximalEndOfFile = filestream.tellp();
+
+	// close the file if the progress bar is complete
+	if(part == nTotal){
+		filestream.close();
+	}
+}
+
