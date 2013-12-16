ca0b1513220d19eadd7ed0c6292ae22e96c14fe2
Carlchristian Eckert <Carli-Eckert@gmx.de>
2013-12-16 11:22:20 +0100

    * scripts for VTK files ready

--- utils/vtktools/convertAll.sh
+++ utils/vtktools/convertAll.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-for i in *.vtk ; do
-  convertVTKtoVTU $i
+if [ "$1" = "" ] ; then
+  echo "give a pattern for the VTK-files! (like dndt_ASE, if the files are called dndt_ASE_XX.vtk)"
+  exit 1
+fi
+
+PATTERN="$1"
+FILE="animation_$1.pvd"
+
+echo '<?xml version"1.0"?>' > $FILE
+echo '<VTKFile type="Collection" version="0.1">' >> $FILE
+echo '  <Collection>' >> $FILE
+
+j=1;
+
+
+for i in $(ls -v $PATTERN*.vtk)
+do
+  echo converting $i
+  ./convertVTKtoVTU $i
+  OUTPUT="$(basename -s .vtk $i)"
+  echo '    <DataSet timestep="'$j'" file="'$OUTPUT'.vtu"/>' >> $FILE
+  j=$(($j+1))
 done
 
-
+echo '  </Collection>' >> $FILE
+echo '</VTKFile>' >> $FILE
