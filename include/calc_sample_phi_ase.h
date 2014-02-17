1e659f031288f4a622c6ff3d1c1ec6510e013aec
Carlchristian Eckert <Carli-Eckert@gmx.de>
2014-02-17 16:47:23 +0100

    Comments in calc_sample_phi_ase.cu .h

--- include/calc_sample_phi_ase.h
+++ include/calc_sample_phi_ase.h
@@ -2,45 +2,21 @@
 #define calc_sample_phi_ase_H
 
 #include <curand_mtgp32dc_p_11213.h>
-/* include <cuda_runtime_api.h> */
 #include <mesh.h>
 
 /**
- * @brief Calculates the phi ASE value for the given 
- *        samplepoint. This is done by a monte carlo
- *        simulation with random generated rays with
- *        the direction to the samplepoint.
+ * @brief Calculates the gain sum for the given 
+ *        sample point with or without reflections. This is done by a monte carlo
+ *        simulation with randomly generated rays and
+ *        wavelenghts.
  *
  * @author Erik Zenker
  * @author Carlchristian Eckert
  * @author Marius Melzer
  * @licence GPLv3
  *
- * @param globalState State for random number generation (mersenne twister).
- *                    The state need to be initialized before. See
- *                    http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/MTGP/
- *                    for more information.
- *
- * @param samplePoint the point for which the phi ASE value is calculated
- *
- * @param mesh all information about prism structure. See mesh.h for details.
- *
- * @param indecesOfPrisms tells each thread(ray) on which prism it should start
- *
- * @param importance constant values, calculated by importance sampling.
- *                   Reduces calculation time for this simulation.
- *
- * @param raysPerSample 
- *
- * @param phiAse Solution vector
- *
- * @param sample_i index of samplepoint in the mesh->samples array
- *
- * @param sigmaA
- *
- * @param sigmaE
- *
  **/
+
 __global__ void calcSampleGainSumWithReflection(curandStateMtgp32* globalState, 
 				 const Mesh mesh,
 				 const unsigned* indicesOfPrisms, 
--- src/calc_sample_phi_ase.cu
+++ src/calc_sample_phi_ase.cu
@@ -8,29 +8,95 @@
 
 
 
+/**
+ * @brief get the offset for accessing indicesOfPrisms and numberOfReflectionSlices (slow!).
+ *
+ * @param blockOffset shared memory location that holds the offset for the whole block (4 warps)
+ * @param raysPerSample number of raysPerSample (can be any number higher than raysPerSample/warpsize)
+ * @param globalOffsetMultiplicator is incremented by 1 each time a warp asks for a new workload
+ * @return an unused offset in the global arrays indicesOfPrisms/numberOfReflectionSlices
+ *
+ */
 __device__ unsigned getRayNumberWarpbased(unsigned* blockOffset,unsigned raysPerSample, unsigned *globalOffsetMultiplicator){
+	// if this is warpID 0
 	if((threadIdx.x &31) == 0){
+		//get a new offset for the warp (threadId % 32)
 		blockOffset[(threadIdx.x>>5)] = atomicInc(globalOffsetMultiplicator,raysPerSample);
 	}
 	__syncthreads();
 
+	// multiply blockoffset by 32 (size of warp)
 	return (threadIdx.x &31) + (blockOffset[(threadIdx.x>>5)] <<5) ;
 
 }
+
+/**
+ * @brief get the offset for accessing indicesOfPrisms and numberOfReflectionSlices.
+ *        Warning: works only for a blocksize of 128 threads!
+ *
+ * @param blockOffset shared memory location that holds the offset for the whole block
+ * @param raysPerSample number of raysPerSample (can be any number higher than raysPerSample/blocksize)
+ * @param globalOffsetMultiplicator is incremented by 1 each time a block asks for a new workload
+ * @return an unused offset in the global arrays indicesOfPrisms/numberOfReflectionSlices
+ *
+ */
 __device__ unsigned getRayNumberBlockbased(unsigned* blockOffset,unsigned raysPerSample,unsigned *globalOffsetMultiplicator){
+	// The first thread in the threadblock increases the globalOffsetMultiplicator (without real limit) 
 	if(threadIdx.x == 0){
+		//blockOffset is the new value of the globalOffsetMultiplicator
 		blockOffset[0] = atomicInc(globalOffsetMultiplicator,raysPerSample);
 	}
 	__syncthreads();
 
+	//multiply blockOffset by 128 (size of the threadblock) 
 	return threadIdx.x + (blockOffset[0] <<7) ;
-
 }
 
+/**
+ * @brief get a random number from [0..length)
+ *
+ * @param length the maximum number to return (exclusive)
+ * @param globalState State for random number generation (mersenne twister).
+ *                    The state need to be initialized before. See
+ *                    http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/MTGP/
+ *                    for more information.
+ *
+ * @return a random number
+ *
+ */
 __device__ __inline__ unsigned genRndSigmas(unsigned length, curandStateMtgp32* globalState) {
   return unsigned(curand_uniform(&globalState[blockIdx.x])*(length-1));
 }
 
+/**
+ * @brief: calculate the sum of all gains for a single sample point s_i, accounting
+ *		   for reflections on the top and bottom surface. Different cladding, 
+ *		   coating and refractive indices are stored in the mesh.
+ *
+ * @param globalState State for random number generation (mersenne twister).
+ *                    The state need to be initialized before. See
+ *                    http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/MTGP/
+ *                    for more information.
+ * @param mesh the gain medium mesh 
+ * @param indicesOfPrisms a mapping from rays to prisms. The value x on the
+ *	      n-th position denotes that ray n is supposed to start propagating 
+ *	      from the x-th prism
+ * @param numberOfReflectionSlices similar to indicesOfPrisms, but denotes the
+ *        reflection slice from which each ray starts (see importance sampling)
+ * @param importance the importance for each prism, obtained through the
+ *        importance sampling kernel
+ * @param raysPerSample the number of rays to use for the sample point s_i
+ * @param gainSum the sum of all gain values for sample point s_i
+ * @param gainSumSquare the sum of all (gain^2) values for sample point s_i
+ *        useful for finding the mean square error
+ * @param sample_i the index of the sample point to sample (s_i)
+ * @param sigmaA the values for absorption cross section in the selected wavelength interval
+ * @param sigmaE the values for emission cross section in the selected wavelength interval
+ * @param maxInterpolation the number of elements contained in sigmaA or sigmaE
+ * @param globalOffsetMultiplicator holds the number of work-requests done by the blocks
+ *        (see getRayNumberBlockbased())
+ *
+ */
 __global__ void calcSampleGainSumWithReflection(curandStateMtgp32* globalState,
 						const Mesh mesh, 
 						const unsigned* indicesOfPrisms, 
@@ -50,26 +116,31 @@ __global__ void calcSampleGainSumWithReflection(curandStateMtgp32* globalState,
   double gainSumTemp = 0;
   double gainSumSquareTemp = 0;
   Point samplePoint = mesh.getSamplePoint(sample_i);
-  __shared__ unsigned blockOffset[4];
+  __shared__ unsigned blockOffset[4]; // 4 in case of warp-based raynumber
 
   // One thread can compute multiple rays
   while (true) {
+	// the whole block gets a new offset (==workload)
 	rayNumber = getRayNumberBlockbased(blockOffset,raysPerSample,globalOffsetMultiplicator);
 	if(rayNumber >= raysPerSample) break;
 
     // Get triangle/prism to start ray from
     unsigned startPrism             = indicesOfPrisms[rayNumber];
-    unsigned reflection_i           = numberOfReflectionSlices[rayNumber]; //numberOfReflectio == ReflectionSlice
+    unsigned reflection_i           = numberOfReflectionSlices[rayNumber];
     unsigned reflections            = (reflection_i + 1) / 2;
     ReflectionPlane reflectionPlane = (reflection_i % 2 == 0) ? BOTTOM_REFLECTION : TOP_REFLECTION;
     unsigned startLevel             = startPrism / mesh.numberOfTriangles;
     unsigned startTriangle          = startPrism - (mesh.numberOfTriangles * startLevel);
     unsigned reflectionOffset       = reflection_i * mesh.numberOfPrisms;
     Point startPoint                = mesh.genRndPoint(startTriangle, startLevel, globalState);
+	
+	//get a random index in the wavelength array
     unsigned sigma_i                = genRndSigmas(maxInterpolation, globalState);
 
     // Calculate reflections as different ray propagations
     double gain    = propagateRayWithReflection(startPoint, samplePoint, reflections, reflectionPlane, startLevel, startTriangle, mesh, sigmaA[sigma_i], sigmaE[sigma_i]);
+
+	// include the stimulus from the starting prism and the importance of that ray
     gain          *= mesh.getBetaValue(startPrism) * importance[startPrism + reflectionOffset];
     
     assert(!isnan(mesh.getBetaValue(startPrism)));
@@ -86,6 +157,32 @@ __global__ void calcSampleGainSumWithReflection(curandStateMtgp32* globalState,
 
 }
 
+/**
+ * @brief: calculate the sum of all gains for a single sample point s_i, ignoring
+ *         reflections. Different cladding, is stored in the mesh.
+ *
+ * @param globalState State for random number generation (mersenne twister).
+ *                    The state need to be initialized before. See
+ *                    http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/MTGP/
+ *                    for more information.
+ * @param mesh the gain medium mesh 
+ * @param indicesOfPrisms a mapping from rays to prisms. The value x on the
+ *	      n-th position denotes that ray n is supposed to start propagating 
+ *	      from the x-th prism
+ * @param importance the importance for each prism, obtained through the
+ *        importance sampling kernel
+ * @param raysPerSample the number of rays to use for the sample point s_i
+ * @param gainSum the sum of all gain values for sample point s_i
+ * @param gainSumSquare the sum of all (gain^2) values for sample point s_i
+ *        useful for finding the mean square error
+ * @param sample_i the index of the sample point to sample (s_i)
+ * @param sigmaA the values for absorption cross section in the selected wavelength interval
+ * @param sigmaE the values for emission cross section in the selected wavelength interval
+ * @param maxInterpolation the number of elements contained in sigmaA or sigmaE
+ * @param globalOffsetMultiplicator holds the number of work-requests done by the blocks
+ *        (see getRayNumberBlockbased())
+ *
+ */
 __global__ void calcSampleGainSum(curandStateMtgp32* globalState,
 				 const Mesh mesh, 
 				 const unsigned* indicesOfPrisms, 
@@ -104,11 +201,11 @@ __global__ void calcSampleGainSum(curandStateMtgp32* globalState,
   double gainSumTemp = 0;
   double gainSumSquareTemp = 0;
   Point samplePoint = mesh.getSamplePoint(sample_i);
-  __shared__ unsigned blockOffset[4];
-
+  __shared__ unsigned blockOffset[4]; // 4 in case of warp-based raynumber
+  
   // One thread can compute multiple rays
-  // The current ray which we compute is based on the gid and an offset (number of threads*blocks)
   while(true){
+	// the whole block gets a new offset (==workload)
     rayNumber = getRayNumberBlockbased(blockOffset,raysPerSample,globalOffsetMultiplicator);
     if(rayNumber>=raysPerSample) break;
 
@@ -118,12 +215,16 @@ __global__ void calcSampleGainSum(curandStateMtgp32* globalState,
     unsigned startTriangle          = startPrism - (mesh.numberOfTriangles * startLevel);
     Point startPoint                = mesh.genRndPoint(startTriangle, startLevel, globalState);
     Ray ray                         = generateRay(startPoint, samplePoint);
+
+	// get a random index in the wavelength array
     unsigned sigma_i                = genRndSigmas(maxInterpolation, globalState);
     assert(sigma_i < maxInterpolation);
 
+	// calculate the gain for the whole ray at once
     double gain    = propagateRay(ray, &startLevel, &startTriangle, mesh, sigmaA[sigma_i], sigmaE[sigma_i]);
+    gain          /= ray.length * ray.length; // important, since usually done in the reflection device function
 
-    gain          /= ray.length * ray.length; // important, since usually done in the reflection device function!
+	// include the stimulus from the starting prism and the importance of that ray
     gain          *= mesh.getBetaValue(startPrism) * importance[startPrism];
 
     gainSumTemp       += gain;
