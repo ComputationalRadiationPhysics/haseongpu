cf0da54a95bd383dcd25cf37914849a01470f8ea
erikzenker <erikzenker@hotmail.com>
2014-03-11 11:42:25 +0100

    Added ASE_calc with cladding, better doc for ASE_calc

--- utils/benchmark_template/ASE_calc_clad.m
+++ utils/benchmark_template/ASE_calc_clad.m
@@ -1,13 +1,14 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Laser pump routine %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % ASE calulations main file 16kW
+% longer calculation without pump action possibel for heat generation
+% estimation in the cladding
 %
 % @author Daniel Albach
-% @date   2009-05-26
+% @date   2011-05-03
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Octave initialization %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% initialization %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear all;
 tic
 isOctave = exist('OCTAVE_VERSION') ~= 0;
@@ -15,10 +16,9 @@ if (isOctave)
    page_output_immediately(1);
 end
 
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Definitions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Crystal parameter
+% Crystal parameter 
 crystal.doping = 2;
 crystal.length = 0.7;    % [cm]
 crystal.tfluo = 9.5e-4;  % 1.0e-3 pour DA
@@ -29,47 +29,53 @@ crystal.levels = 10;
 steps.time = 100;
 
 % Pump parameter
-pump.s_abs = 0.76e-20;    % Absorption cross section in cm² (0.778e-20 pour DA)
-pump.s_ems = 0.220e-20;   % Emission cross section in cm²(0.195e-20 pour DA)
-pump.I = 16e3;            % Pump intensity in W/cm²
-pump.T = 1e-3;            % Pump duration in s
-pump.wavelength = 940e-9; % Pump wavelength in m
-pump.r = 1.5;             % Pump radius
-pump.exp = 20;            % Pump exponent
+pump_stretch = 1;
+pump_aspect = 1;
+pump_diameter = 3;
+pump.s_abs = 0.778e-20;                % Absorption cross section in cm² (0.778e-20 pour DA)
+pump.s_ems = 0.195e-20;                % Emission cross section in cm²(0.195e-20 pour DA)
+pump.I = 16e3;                         % Pump intensity in W/cm²
+pump.T = 1e-3;                         % Pump duration in s
+pump.wavelength = 940e-9;              % Pump wavelength in m
+pump.ry = pump_diameter/2*pump_aspect;
+pump.rx = pump_diameter/2;             % Pump radius
+pump.exp = 40;                         % Pump exponent
 
 % Laser parameter
-laser.s_abs = load('sigma_a.txt'); % Absorption spectrum cm2(1.16e-21 pour DA)
-laser.s_ems = load('sigma_e.txt'); % Emission spectrum in cm2(2.48e-20)
-laser.I = 1e6;                     % Laser intensity
-laser.T = 1e-8;                    % Laser duration
-laser.wavelength = 1030e-9;        % Lasing wavelength for max emission in m
+laser.s_abs = load('sigma_a.txt');      % Absorption spectrum cm2(1.16e-21 pour DA)
+laser.s_ems = load('sigma_e.txt');      % Emission spectrum in cm2(2.48e-20)
+laser.I = 1e6;                          % Laser intensity
+laser.T = 1e-8;                         % Laser duration
+laser.wavelength = 1030e-9;             % Lasing wavelength in m
 [laser.max_ems, i] = max(laser.s_ems);
 laser.max_abs = laser.s_abs(i);
 
 % Mode parameter
-mode.BRM = 1; % 1 Backreflection, 0 Transmissionmode
-mode.R = 1;   % reflectivity of the coating
+mode.BRM = 1;  % 1 Backreflection, 0 Transmissionmode
+mode.R = 1;    % reflectivity of the coating
+mode.extr =0;  %no extraction!!
 
 % Constants
 const.N1per = 1.38e20;
 const.c = 3e8;
 const.h = 6.626e-34;
-NumRays = 1e5;
+NumRays = 1e4;
 NumRays = int32(NumRays);
 N_tot = const.N1per*crystal.doping;
+Ntot_gradient = zeros(crystal.levels, 1);      % doping gradient if not given by load!
+Ntot_gradient(:) = crystal.doping*const.N1per;
 mesh_z = crystal.levels;
 z_mesh = crystal.length/(mesh_z-1);
 timeslice = 50;
-timetotal = 1e-3;                    %[s]
+timeslice_tot = 150;
+timetotal = 1e-3; %[s]
 time_t = timetotal/timeslice;
 
 % Constants for short use
-c = const.c; % m/s
-h = const.h; % Js
-
+c = const.c; %m/s
+h = const.h; %Js
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Create mesh %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 % Load the grid from file
 % the grid is done using the distmesh routines
 % use e.g. cr_60mm_30mm.m in meshing folder
@@ -87,13 +93,14 @@ beta_vol = zeros(N_cells,mesh_z-1);
 % In first timeslice phi_ASE stays 0
 phi_ASE=zeros(size(p,1),mesh_z);
 dndt_ASE(:,:) = phi_ASE(:,:);
+flux_clad = phi_ASE(:,:);
 dndt_pump = (beta_cell);
 
 % Save values in file -> first timestep the phi_ASE is zero
 vtk_wedge('beta_cell_0.vtk',beta_cell, p, t_int, mesh_z, z_mesh);
 vtk_wedge('dndt_pump_0.vtk',dndt_pump, p, t_int, mesh_z, z_mesh);
 vtk_wedge('dndt_ASE_0.vtk',dndt_ASE , p, t_int, mesh_z, z_mesh);
-save save_0.mat
+save data_0.mat
 
 temp = pump.T;
 time_beta = 1e-6;
@@ -107,8 +114,8 @@ intensity = pump.I;
 for i_p=1:size(p,1)
    beta_crystal=beta_cell(i_p,:);
    pulse = zeros(steps.time,1);
-   pump.I = intensity*exp(-(sqrt(p(i_p,1)^2+p(i_p,2)^2)/pump.r)^pump.exp);
-   [beta_crystal,beta_store,pulse,Ntot_gradient] = beta_int(beta_crystal,pulse,const,crystal,steps,pump,mode);
+   pump.I = intensity*exp(-sqrt(p(i_p,1)^2/pump.ry^2+p(i_p,2)^2/pump.rx^2)^pump.exp);
+   [beta_crystal,beta_store,pulse,Ntot_gradient] = beta_int3(beta_crystal,pulse,const,crystal,steps,pump,mode,Ntot_gradient);
    beta_c_2(i_p,:)=beta_crystal(:);
 end
 
@@ -132,10 +139,47 @@ for i_p=1:size(p,1)
     end
 end
 
+vtk_wedge('beta_cell_1.vtk',beta_cell, p, t_int, mesh_z, z_mesh);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Definition of cladding %%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Which index is the cladding?
+clad_number = 1;
+clad_number = int32(clad_number);
+
+% Absorption of the cladding
+clad_abs = 5.5;
+
+% Prepare which part is concerning the cladding and which one is the Yb doped part
+% this part mus be adapted for each case!
+clad_index = find(clad==clad_number);
+Yb_index = find(clad~=clad_number);
+
+% the logic is inverse, which index to delete ;)
+% this represents the triangle index and using the p-variable, you get the points...
+Yb_points_t = t;
+Yb_points_t(clad_index,:) = [];
+Yb_points_t2 = reshape(Yb_points_t,[],1);
+clear Yb_points_t;
+Yb_points_t = unique(Yb_points_t2);
+clear Yb_points_t2;
+
+clad_points_t = t;
+clad_points_t(Yb_index,:) = [];
+clad_points_t2 = reshape(clad_points_t,[],1);
+clear clad_points_t;
+clad_points_t = unique(clad_points_t2);
+clear clad_points_t2;
+
+% Now get the lengths of the 
+length_clad = size(clad_points_t,1);
+length_Yb = size(Yb_points_t,1);
+
+% Make sure that clad is integer variable
+clad_int = int32(clad);
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Main pump loop %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-for i_slice=1:timeslice-1
-    disp(['']);
+for i_slice=1:timeslice_tot-1
     disp(['TimeSlice ' num2str(i_slice) 'calculation started']);
     % ******************* BETA PUMP TEST ******************************
     % make a test with the gain routine "gain.m" for each of the points
@@ -150,6 +194,7 @@ for i_slice=1:timeslice-1
     file_b = ['beta_cell_' num2str(i_slice) '.vtk'];
     file_p = ['dndt_pump_' num2str(i_slice) '.vtk'];
     file_A = ['dndt_ASE_' num2str(i_slice) '.vtk'];
+    file_C = ['flux_clad_' num2str(i_slice) '.vtk'];
     
     vtk_wedge(file_b,beta_cell, p, t_int, mesh_z, z_mesh);
     vtk_wedge(file_p,dndt_pump, p, t_int, mesh_z, z_mesh);
@@ -180,58 +225,70 @@ for i_slice=1:timeslice-1
 
         v = vertcat(v_1,v_2);
 
-        if (isOctave)
-          beta_vol(:,i_z) = griddata3(x,y,z,v,xi,yi,zi);
-        else
-          beta_vol(:,i_z) = griddata(x,y,z,v,xi,yi,zi);
-        end
+        beta_vol(:,i_z) = griddata3(x,y,z,v,xi,yi,zi);
 
         z = z + z_mesh;
         zi = zi + z_mesh;
     end
+
+
     %%%%%%%%%%%%%%%%%%%%%%%%%%%% Call external ASE application %%%%%%%%%%%%%%%%%%%%
+     [phi_ASE, mse_values, N_rays] = calcPhiASE(p,t_int,beta_cell,beta_vol,normals_x,normals_y,sorted_int,surface,x_center,y_center,normals_p,forbidden, NumRays, N_tot, z_mesh,laser,crystal,mesh_z,i_slice,clad_int,clad_number,clad_abs);
 
-    [phi_ASE, mse_values, N_rays] = calcPhiASE(p,t_int,beta_cell,beta_vol,normals_x,normals_y,sorted_int,surface,x_center,y_center,normals_p,forbidden, NumRays, N_tot, z_mesh,laser,crystal,mesh_z);
 
-    % Calc dn/dt ASE
-    for i_p=1:size(p,1)
+    % Calc dn/dt ASE, change it to only the Yb:YAG points
+    for i_p=1:length_Yb
         for i_z=1:mesh_z
-	  % Calc local gain (g_l)
-          g_l = -(N_tot*laser.max_abs - N_tot*beta_cell(i_p,i_z)*(laser.max_ems+laser.max_abs));
-          dndt_ASE(i_p,i_z) = g_l*phi_ASE(i_p,i_z)/crystal.tfluo;
+            pos_Yb = Yb_points_t(i_p);
+	    % Calc local gain (g_l)
+            g_l = -(N_tot*laser.s_abs - N_tot*beta_cell(pos_Yb,i_z)*(laser.s_ems+laser.s_abs));
+            dndt_ASE(pos_Yb,i_z) = g_l*phi_ASE(pos_Yb,i_z)/crystal.tfluo;
+        end
+    end
+    
+    
+    % Now for the cladding points
+    for i_p=1:length_clad
+        for i_z=1:mesh_z
+            % Calc local gain (g_l)
+            pos_clad = clad_points_t(i_p);
+            flux_clad(pos_clad,i_z) = clad_abs*phi_ASE(pos_clad,i_z)/crystal.tfluo;
         end
     end
     
     vtk_wedge(file_A,dndt_ASE , p, t_int, mesh_z, z_mesh);
-    
+    vtk_wedge(file_C,flux_clad , p, t_int, mesh_z, z_mesh);
     save(['save_' num2str(i_slice) '.mat']);
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%% Prepare next timeslice %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%% Prepare next timeslice %%%%%%%%%%%%%%%%%%%%%%%%%%%
     temp = pump.T;
     time_beta = 1e-6;
     pump.T = time_beta;
-    
     temp_f = crystal.tfluo;
     crystal.tfluo = 1;
-    
     beta_c_2 = zeros(size(p,1),mesh_z);
-
     intensity = pump.I;
 
+    % Now call beta_int for each of the points
     for i_p=1:size(p,1)
         beta_crystal=beta_cell(i_p,:);
         pulse = zeros(steps.time,1);
-        pump.I = intensity*exp(-(sqrt(p(i_p,1)^2+p(i_p,2)^2)/pump.r)^pump.exp);
-        [beta_crystal,beta_store,pulse,Ntot_gradient] = beta_int(beta_crystal,pulse,const,crystal,steps,pump,mode);
+        pump.I = intensity*exp(-sqrt(p(i_p,1)^2/pump.ry^2+p(i_p,2)^2/pump.rx^2)^pump.exp);
+        [beta_crystal,beta_store,pulse,Ntot_gradient] = beta_int3(beta_crystal,pulse,const,crystal,steps,pump,mode,Ntot_gradient);
         beta_c_2(i_p,:) = beta_crystal(:);
     end
 
-    % Recalculate beta_cell
-    dndt_pump = (beta_c_2 - beta_cell)./time_beta;
+    if (i_slice<timeslice)
+        dndt_pump = (beta_c_2 - beta_cell)./time_beta;
+    else
+        dndt_pump = zeros(size(p,1),mesh_z);
+    end
+    
     pump.I = intensity;
     pump.T = temp;
     crystal.tfluo = temp_f;
+
+    % Recalculate beta_cell
     for i_p=1:size(p,1)
         for i_z=1:mesh_z
             beta_cell(i_p,i_z) = crystal.tfluo*(dndt_pump(i_p,i_z)-dndt_ASE(i_p,i_z))*(1-exp(-time_t/crystal.tfluo))+beta_cell(i_p,i_z)*exp(-time_t/crystal.tfluo);
@@ -239,6 +296,17 @@ for i_slice=1:timeslice-1
     end
 end
 
+file_b = ['beta_cell_' num2str(timeslice_tot) '.vtk'];
+file_p = ['dndt_pump_' num2str(timeslice_tot) '.vtk'];
+file_A = ['dndt_ASE_' num2str(timeslice_tot) '.vtk'];
+file_C = ['flux_clad_' num2str(timeslice_tot) '.vtk'];
+
+vtk_wedge(file_b,beta_cell, p, t_int, mesh_z, z_mesh);
+vtk_wedge(file_p,dndt_pump, p, t_int, mesh_z, z_mesh);
+vtk_wedge(file_A,dndt_ASE , p, t_int, mesh_z, z_mesh);
+vtk_wedge(file_C,flux_clad , p, t_int, mesh_z, z_mesh);
+
 disp('Calculations finished');
 toc
 
+% Obviously I forgot to save the latest results, must be added!
