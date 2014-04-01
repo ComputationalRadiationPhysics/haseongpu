cfd0b2230b7672a3d9a31214d03d5b3f59725bab
erikzenker <erikzenker@hotmail.com>
2014-04-01 13:03:05 +0200

    Added version file with release version 0.3 as first version

---
+++ include/version.h
@@ -0,0 +1,27 @@
+#pragma once
+/**
+ * Copyright 2013 Erik Zenker, Carlchristian Eckert, Marius Melzer
+ *
+ * This file is part of HASENonGPU
+ *
+ * HASENonGPU is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HASENonGPU is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HASENonGPU.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// HASEN_ON_GPU_VERSION % 100 is sub-minor version 
+// HASEN_ON_GPU_VERSION / 100 % 1000 is minor version
+// HASEN_ON_GPU_VERSION / 100000 is major version
+
+#define HASEN_ON_GPU_VERSION 003000
+
