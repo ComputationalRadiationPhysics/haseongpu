83d2142b2c8ab7ff62fa06123473c529ecf25f54
Carlchristian Eckert <Carli-Eckert@gmx.de>
2015-06-25 15:49:46 +0200

    Changed char* to string, added attribution+license

--- src/types.cc
+++ src/types.cc
@@ -1,8 +1,34 @@
+/**
+ * Copyright 2015 Erik Zenker, Carlchristian Eckert
+ *
+ * This file is part of HASEonGPU
+ *
+ * HASEonGPU is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HASEonGPU is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HASEonGPU.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #include "types.hpp"
 #include <iostream>
+#include <string>
 
+// Idea from http://stackoverflow.com/questions/3342726/c-print-out-enum-value-as-text
+// Answer by user SigTerm
+// Switched char* to std::string
+//
 std::ostream& operator<<(std::ostream& out, const DeviceMode value){
-    const char* s = 0;
+    std::string s;
 #define PROCESS_VAL(p) case(p): s = #p; break;
     switch(value){
         PROCESS_VAL(NO_DEVICE_MODE);
@@ -14,7 +40,7 @@ std::ostream& operator<<(std::ostream& out, const DeviceMode value){
 }
 
 std::ostream& operator<<(std::ostream& out, const ParallelMode value){
-    const char* s = 0;
+    std::string s;
 #define PROCESS_VAL(p) case(p): s = #p; break;
     switch(value){
         PROCESS_VAL(NO_PARALLEL_MODE);
